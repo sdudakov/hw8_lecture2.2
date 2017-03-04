60e310904ee647400ecbb5a6b96c34688629398e
Сергей Дудаков <grey@kmail.ru>
2017-03-04 17:14:07 +0300

    1ый рабочий варинат скрипта для файлов newsafr.json и newscy.json

--- script_hw8.py
+++ script_hw8.py
@@ -1,8 +1,8 @@
 news_files={
       "newsafr.json": "utf-8",
       "newscy.json": "koi8-r",
-      # "newsfr.json": "iso8859_5", ? 
-      # "newsit.json": "cp1251"
+      # "newsfr.json": "iso8859_5", #не разобрался какая у файла кодировка
+      # "newsit.json": "cp1251",  #у этого файла немного другой формат 
 }
 
 
@@ -14,12 +14,12 @@ def news_country(file_name, code_page):
 		data = json.load(file)
 		return data['rss']['channel']['title']
 
-#из всех заголовков и новостей сделать один список news_list_6char из слов длиннее 6 символов
+#из всех заголовков и новостей сделать один список news_list из слов длиннее 6 символов
 def news_list(file_name, code_page):
 	with open(file_name, encoding=code_page) as file:
 		data = json.load(file)
 		news_list_all = [] #создаю пустой список для всех слов новостей
-		news_list_6char = [] #создаю пустой список для слов более 6 символов
+		news_list = [] #создаю пустой список для слов более 6 символов
 		for item in data['rss']['channel']['item']:
 			title = item['title']['__cdata']
 			description = item['description']['__cdata']
@@ -29,8 +29,8 @@ def news_list(file_name, code_page):
 			news_list_all += description #и description
 			for _ in news_list_all:
 				if len(_) > 6:
-					news_list_6char.append(_)
-		return news_list_6char
+					news_list.append(_)
+		return news_list
 	
 #создаем словарь из слов в котором подсчитаем сколько раз слово из списка встречается в этом списке
 def analysis(news_list):
@@ -42,9 +42,23 @@ def analysis(news_list):
 			words_count[word] = 1
 	return words_count
 
-#print(news_country("newsafr.json", "utf-8"))
-words_count = analysis(news_list("newsafr.json", "utf-8"))
-#print(news_set(news_list("newsafr.json", "utf-8")))
-words_sorted = sorted(words_count.items(),key=lambda x:x[1],reverse=True) #эту конструкцию подсмотрел в Интернете
 
-print(words_sorted)
+#сортируем словарь из слов, что бы первыми элементами были самые частовстречающиеся слова, печатаем 10 таких слов
+def top_ten_words(words_count):
+	words_sorted = sorted(words_count.items(),key=lambda x:x[1],reverse=True) #эту конструкцию подсмотрел в Интернете
+	for _ in range(10):
+		word, count = words_sorted[_]
+		print(word)
+
+
+#выводим название страны и топ 10 слов из новостей
+def print_top_ten_words(news_files):
+	for file in news_files:
+		codepage = news_files[file]
+		country = news_country(file, codepage)
+		news = news_list(file, codepage)
+		words_count = analysis(news)
+		print("\n{}\nДесять наиболее часто встречающихся слов:" .format(country))
+		top_ten_words(words_count)
+
+print_top_ten_words(news_files)
